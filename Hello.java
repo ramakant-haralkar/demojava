569e34dfc24c4d24e864282adc70aa772f9298e9
Ramakant Haralkar <32733802+ramakant-haralkar@users.noreply.github.com>
2019-09-23 11:46:07 +0530

    Update Hello.java
    date added

--- Hello.java
+++ Hello.java
@@ -1,8 +1,14 @@
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();  
 		System.out.println("Demo java new commit");
+		System.out.println("Today: "+dtf.format(now));
 
 	}
 
