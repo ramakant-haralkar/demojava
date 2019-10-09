c1231139f5422f4e6218cfef35ce8e59f253df34
ramakant.haralkar@gmail.com <ramakant.haralkar@gmail.com>
2019-10-09 20:47:36 +0530

    2nd commit

--- Hello.java
+++ Hello.java
@@ -8,7 +8,8 @@ public class Hello {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
 		System.out.println("Demo java new commit");
-		System.out.println("Demo. java new commit 09-10-2019");
+		System.out.println("Demo java new commit 09-10-2019");
+		System.out.println("Demo java new commit 2nd 09-10-2019");
 		System.out.println("Today: "+dtf.format(now));
 
 	}
