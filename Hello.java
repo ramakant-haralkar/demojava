043dfcae042e76c74c2081c7e6987acfd0edef14
ramakant.haralkar@gmail.com <ramakant.haralkar@gmail.com>
2019-10-13 17:55:06 +0530

    SYSOUT DELETED

--- Hello.java
+++ Hello.java
@@ -7,9 +7,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
-		System.out.println("Demo java new commit");
-		System.out.println("Demo java new commit 09-10-2019");
-		System.out.println("Demo java new commit 2nd 09-10-2019");
+		
 		System.out.println("Today: "+dtf.format(now));
 
 	}
